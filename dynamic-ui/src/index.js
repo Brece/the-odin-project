a4e9df6b784909485efd84ca899d974f948c7d9d
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2021-12-30 16:35:09 +0100

    adds html template and styles for Image Slider

--- dynamic-ui/src/index.js
+++ dynamic-ui/src/index.js
@@ -1,8 +1,10 @@
 import './styles/style.css';
 import { dropDown } from './modules/dropDown-module';
 import { mobileMenu } from './modules/mobileMenu-module';
+import { imageSlider } from './modules/imageSlider-module';
 
 document.addEventListener('DOMContentLoaded', () => {
     dropDown();
     mobileMenu();
+    imageSlider();
 });
---
+++ dynamic-ui/src/modules/imageSlider-module.js
@@ -0,0 +1,24 @@
+/**
+ *     <section class="c-slider">
+      <div class="c-slider__content">
+        <div class="c-slider__content__arrow"><i class="fas fa-chevron-left c-slider__content__arrow__left"></i></div>
+        <div class="c-slider__content__img">
+          <img src="#" alt="">
+        </div>
+        <div class="c-slider__content__arrow"><i class="fas fa-chevron-right c-slider__content__arrow__right"></i></div>
+      </div>
+      <div class="c-slider__nav">
+        <div class="c-slider__nav__dot"><div class="c-slider__nav__dot__fill"></div></div>
+        <div class="c-slider__nav__dot"><div class="c-slider__nav__dot__fill"></div></div>
+        <div class="c-slider__nav__dot"><div class="c-slider__nav__dot__fill"></div></div>
+        <div class="c-slider__nav__dot"><div class="c-slider__nav__dot__fill"></div></div>
+        <div class="c-slider__nav__dot"><div class="c-slider__nav__dot__fill"></div></div>
+      </div>
+    </section>
+ */
+
+function imageSlider() {
+
+}
+
+export { imageSlider };
