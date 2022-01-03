bfa7755f2306f851bbddc01c52e2f4f908a6366b
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-01-03 20:24:46 +0100

    changed from async/await to Promise notation

--- api/app.js
+++ api/app.js
@@ -1,23 +1,58 @@
-const image = document.querySelector('.c-image');
+const images = document.querySelectorAll('.c-image');
 const btn = document.querySelector('.c-btn');
 const search = document.querySelector('#text');
 const errMsg = document.querySelector('.c-error');
 
-btn.addEventListener('click', function(e) {
+btn.addEventListener('click', (e) => {
     e.preventDefault();
-    if (search.value.trim() !== "") {
-        loadImage(search.value);
-    }
+    loadGallery();
 });
 
-async function loadImage(search) {
+/*
+async function getImage(search) {
     try {
         const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zcsDt37h7nEVAe3YpEOd6Kgse2Suv0oV&s=${search}`, {mode: 'cors'});
         const data = await response.json();
-        image.src = data.data.images.original.url;
         errMsg.textContent = '';
+        return data;
     } catch (err) {
-        errMsg.textContent = `We couldn't find any images for your search request`;
+        errMsg.textContent = `We couldn't find any images for your search request.`;
         console.error(err);
     }
 }
+*/
+
+function getData(search) {
+    return new Promise((resolve, reject) => {
+            fetch(`https://api.giphy.com/v1/gifs/translate?api_key=zcsDt37h7nEVAe3YpEOd6Kgse2Suv0oV&s=${search}`, {mode: 'cors'})
+            .then((resp) => resp.json())
+            .then((data) => resolve(data))
+            .catch((err) => {
+                errMsg.textContent = `We couldn't find any images for your search request.`;
+                new Error(err);
+            });
+    });
+}
+
+function loadGallery() {
+    if (search.value.trim() !== "") {
+        let dataRequests = [];
+        errMsg.textContent = '';
+
+        images.forEach((image) => {
+            dataRequests.push(getData(search.value));
+        });
+
+        Promise.all(dataRequests)
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) {
+                    images[i].src = data[i].data.images.original.url;
+                }
+            })
+            .catch((err) => console.log(err));
+    } else {
+        errMsg.textContent = 'Please type in your search.';
+    }
+}
+
+//Giphy returns same urls for "translate" function, for different images use "search" and set a "limit" for obtained objects
