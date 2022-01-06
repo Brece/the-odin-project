0950c58cdf9fc81ad4211a51517246c008080a28
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-01-06 01:08:39 +0100

    adds Jest and JS file setup

--- testing-practice/src/index.js
+++ testing-practice/src/index.js
@@ -1,3 +1,7 @@
 import './styles/style.css';
+import { capitalize } from './js/capitalize';
+import { caesarCipher } from './js/caesarCipher';
+import { calculator } from './js/calculator';
+import { reverseString } from './js/reverseString';
 
 console.log("xxx yyy");
