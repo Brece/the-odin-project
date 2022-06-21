27ace28612be65ced79bf0f88c6ba07604a4da28
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-06-21 23:00:38 +0200

    adds pages, static and dynamic routing

---
+++ node-basic-site-express/src/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+// middleware for custom favicon
+const favicon = require('serve-favicon');
+app.use(favicon(path.join(
+    __dirname,
+    'favicon.ico'
+    )));
+
+// routes
+let basePath = path.join(
+    __dirname,
+    'pages'
+);
+
+app.get('/', (req, res) => {
+    res.sendFile(basePath + '/' + 'index.html');
+});
+
+app.get('/:name', (req, res) => {
+    let filepath = basePath + '/' + `${req.params.name}.html`;
+
+    fs.readFile(filepath, (err, data) => {
+        // file does not exist
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.sendFile(basePath + '/' + '404.html');
+            } else {
+                res.send(`Server Error: ${err.code}`);
+            }
+        } else {
+            // success
+            res.end(data);
+            // res.sendFile(filepath);
+        }
+    })
+});
+
+app.listen(port);
